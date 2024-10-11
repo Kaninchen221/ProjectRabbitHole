0af82ec5956a38fdab31d4ed60a0e0ae16c88ba0
Kaninchen221 <pawel8krolik@gmail.com>
2024-10-11 15:18:21 +0200

    Mark as tool to see the effect in the editor

--- Content/Actors/Light/PulsatingLight.gd
+++ Content/Actors/Light/PulsatingLight.gd
@@ -1,16 +1,13 @@
+@tool
 extends PointLight2D
 
 @export var curve: Curve
 @export_range(0, 1) var min_light_energy : float
 @export_range(0, 1) var max_light_energy : float
-@export_range(0, 1) var time : float
 @export_range(-1, 1, 2) var direction_light_energy : float
 @export var pause : bool = true
 @export_range(0.0, 100.0) var time_modifier : float
-
-var diff_light_energy
-func _ready() -> void:
-	diff_light_energy = max_light_energy - min_light_energy
+@export_range(0, 1) var time : float
 
 func _process(delta: float) -> void:
 	if time >= 1:
@@ -23,7 +20,8 @@ func _process(delta: float) -> void:
 	if not pause:
 		time += delta * time_modifier * direction_light_energy
 	
+	set_energy(calculate_light_energy())
+
+func calculate_light_energy():
 	var sampled_value = curve.sample(time)
-	var current_light_energy = sampled_value * diff_light_energy + min_light_energy
-	
-	set_energy(current_light_energy)
+	return sampled_value * (max_light_energy - min_light_energy) + min_light_energy
