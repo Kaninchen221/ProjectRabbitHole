567683eba1048134639ef1a3622519215689d807
Kaninchen221 <pawel8krolik@gmail.com>
2024-10-16 22:39:47 +0200

    Move fps counter to player debug menu

--- Content/Actors/Characters/Player/Scripts/PlayerDebugMenu.gd
+++ Content/Actors/Characters/Player/Scripts/PlayerDebugMenu.gd
@@ -4,6 +4,8 @@ func _process(_delta):
 	if not ImGui.Begin("Player Debug Menu"):
 		return
 		
+	ImGui.Text("FPS: %d" % Engine.get_frames_per_second())
+		
 	if ImGui.Button("Send test message to nearest user"):
 		%ChatUser.send_message_to_nearest_user("Test Message")
 		
