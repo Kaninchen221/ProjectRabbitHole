f45c28bb67351f38a4c5bc89683f4808f0f616db
Kaninchen221 <pawel8krolik@gmail.com>
2024-10-19 12:35:25 +0200

    Add scale to camera zoom and attach it to the DayNight node in player

---
+++ Content/Scripts/ScaleToCameraZoom.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export
+var source : Camera2D
+
+@onready 
+var previous_zoom : Vector2 = source.get_zoom()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if previous_zoom == source.get_zoom():
+		return
+
+	var diff = source.get_zoom() - previous_zoom
+	set_scale(get_scale() + diff)
+	previous_zoom = get_scale()
